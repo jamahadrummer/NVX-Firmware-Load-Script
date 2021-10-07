23b9b07b3a220078f6c118efa60c723819a93e2f
jamahadrummer <jamahadrummer@live.com>
2021-10-07 15:30:11 -0500

    currently broken

--- nvx-fw-update-pwsh.ps1
+++ nvx-fw-update-pwsh.ps1
@@ -34,6 +34,7 @@ Write-Host @"
 
 $stopwatch = [system.diagnostics.stopwatch]::StartNew()
 Import-Module PSCrestron
+$VerbosePreference = "continue"
 $ResultsTable = @()
 ###############################################################
 ##Enter admin creditionals here#####
@@ -44,11 +45,12 @@ $deviceList = Get-AutoDiscovery -Pattern nvx -ShowProgress
 ###############################################################
 ##Enter full path to firmware file and version number here#####
 ###############################################################
-$fw = "G:\My Drive\Abt Commercial Audio Video\Danny Jama\Updating\Crestron\Tippy PSCrestron Superscripts\Crestron-EDK-Superscripts-main 2021_02_17\dm-nvx-35x-enc_6.0.4835.00027_r416525.zip"
+$fw = "G:\My Drive\Abt Commercial Audio Video\Danny Jama\Updating\Crestron\NVX Firmware Load Script Toolbox\dm-nvx-35x-enc_6.0.4835.00027_r416525.zip"
 $fwVersion = '6.0.4835.00027'
 ###############################################################
-# start the jobs
-foreach ($dev in $deviceList) {
+##Define Scripts for jobs######################################
+###############################################################
+$fwCheck = {
     write-host $dev
     'Checking If Update Is Needed'
     if ($dev.Description -match ($fwVersion)) {
@@ -59,14 +61,10 @@ foreach ($dev in $deviceList) {
         Write-Host 'Unit Needs Update'
         $updateNeed = 'True'
         write-host 'Sending update'
-
         Send-CrestronFirmware -Device $dev.ip -LocalFile $fw -Secure -Username $uName -Password $pWord
     } 
-}
-
-foreach ($dev in $deviceList) {
     if ($updateNeed -match ('True')) {
-        Read-Host -Prompt “Press Enter When Ready To Update”
+        # Read-Host -Prompt “Press Enter When Ready To Update”
         #Invoke-CrestronCommand -Device $dev -Command imgupd -Password $pWord -Secure -Username $uName
         Write-Host 'IMGUPD Command Sent'
     }
@@ -74,7 +72,22 @@ foreach ($dev in $deviceList) {
         write-host $dev
         'Update not Sent, Check Version/Connection'
     }
+} 
+
+
+# start the jobs
+
+foreach ($dev in $deviceList) {
+    Start-Job -ScriptBlock $fwCheck -Name $dev 
 }
+
+Receive-Job
+
+# while((Get-Job -State Running).count){
+#     Get-Job | Where-Object {$_.State -eq 'Complete' -and $_.HasMoreData} | ForEach-Object {Receive-Job $_}
+#     start-sleep -seconds 1
+# }
+
 
 # Return the results
 #-------------------
